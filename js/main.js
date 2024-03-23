015a3ed840dc2a063f4de15fff0391ed2d700b88
dinhhaidh <phamhaiqt02@gmail.com>
2024-03-23 16:06:11 +0700

    slider ưeb

---
+++ js/main.js
@@ -0,0 +1,17 @@
+let swiperHome = new Swiper('.home__swiper', {
+    loop: true,
+    spaceBetween: -24,
+    slidesPerView: 'auto',
+    centeredSlides: 'auto',
+
+    autoplay: {
+        delay: 3000,
+        disableOnInteraction: false,
+    },
+
+    breakpoints: {
+        1220: {
+            spaceBetween: -32,
+        }
+    }
+});
